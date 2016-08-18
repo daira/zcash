2012763b730f2388946430ba9e710319aef8b04b
Sean Bowe <ewillbefull@gmail.com>
2016-08-18 12:17:27 -0600

    Update libsnark dependency.

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1
 $(package)_download_path=https://github.com/zcash/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=369fe9f5a8018eb46cce3cf50bfbf21419d8e963b25067ffd00cb9ac5c39f649
-$(package)_git_commit=11242d8afe304c8bcc252baec483f04a110d1dd4
+$(package)_sha256_hash=97daf1f93ac375dfd643f912922d5693278efeeabb7ee4ca876bb95c30c60660
+$(package)_git_commit=8ae419bb79d3a0aeaf56642490e9d05100002edf
 
 $(package)_dependencies=libgmp libsodium
 
