852af461274651160f840e655b64520a2d5b43e0
Daira Hopwood <daira@jacaranda.org>
2016-10-20 05:23:17 +0100

    Move Tromp's solver to src/pow/tromp.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/miner.cpp
+++ src/miner.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "miner.h"
-#include "trompequihash/equi_miner.h"
+#include "pow/tromp/equi_miner.h"
 
 #include "amount.h"
 #include "chainparams.h"
