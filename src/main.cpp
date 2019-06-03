356d739c0402fe292b678b5a2094db9b443373f6
Aditya Kulkarni <adityapk@gmail.com>
2019-06-03 13:47:34 -0700

    Add checkpoint support

--- src/main.cpp
+++ src/main.cpp
@@ -3899,12 +3899,9 @@ bool ContextualCheckBlock(
     // If this is initial block download and "ibdskiptxverification" is set, we'll skip verifying the transactions
     if (IsInitialBlockDownload(chainparams) && GetBoolArg("-ibdskiptxverification", false)) {
         // If checkpoints are enabled, then skip verification only upto the last checkpoint.
-        if (fCheckpointsEnabled) {
-            CBlockIndex *pindexLastCheckpoint = Checkpoints::GetLastCheckpoint(chainparams.Checkpoints());
-            if (pindexLastCheckpoint && pindexLastCheckpoint->GetAncestor(pindexPrev->nHeight) == pindexPrev) {
-                // This block is connecting to an ancestor of a checkpoint: skip the Tx checks.
+        if (fCheckpointsEnabled && 
+            nHeight < Checkpoints::GetTotalBlocksEstimate(chainparams.Checkpoints())) {
                 return true;
-            }
         }
     }
 
